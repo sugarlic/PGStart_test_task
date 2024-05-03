e867b0487a6b80d215db53af46c299a3fe39337c
sullustd <sullustd@student.21-school.ru>
2024-05-03 17:08:24 +0300

    update docker-compose

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"os"
 
-	_ "github.com/jackc/pgx/v5"
+	_ "github.com/lib/pq"
 	"github.com/test/pkg/models/postgre"
 )
 
@@ -22,20 +22,20 @@ func main() {
 	dsn := flag.String("dsn", "host=db port=5432 user=postgres password=1 dbname=postgres sslmode=disable", "Название PostgreSQL источника данных")
 	flag.Parse()
 
-	infoLogFile, err := os.OpenFile("./log/info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("error opening info log file: %v", err)
-	}
-	defer infoLogFile.Close()
+	// infoLogFile, err := os.OpenFile("./log/info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	log.Fatalf("error opening info log file: %v", err)
+	// }
+	// defer infoLogFile.Close()
 
-	errorLogFile, err := os.OpenFile("./log/error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("error opening error log file: %v", err)
-	}
-	defer errorLogFile.Close()
+	// errorLogFile, err := os.OpenFile("./log/error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	log.Fatalf("error opening error log file: %v", err)
+	// }
+	// defer errorLogFile.Close()
 
-	infoLog := log.New(infoLogFile, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(errorLogFile, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	db, err := openDB(*dsn)
 	if err != nil {
